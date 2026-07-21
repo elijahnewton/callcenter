ddc799cd2eb7faf9aac731e7532f4bdf748c7a79
ELIJAH MUSIITWA <65656407+elijahnewton@users.noreply.github.com>
2026-07-21 18:36:03 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 import * as XLSX from "xlsx";
-import { GroupDurableObject } from "./GroupDo";
+export { GroupDurableObject } from "./GroupDo";
 import { authenticateRequest, syncUserToD1 } from "./lib/auth";
 
 type Bindings = {
